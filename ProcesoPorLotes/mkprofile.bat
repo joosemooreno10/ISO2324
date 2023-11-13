75e4288526dfaa96ba42da3d69b738d3128b5c9c
joosemooreno10 <146065095+joosemooreno10@users.noreply.github.com>
2023-11-13 12:28:06 +0100

    Add files via upload

---
+++ ProcesoPorLotes/mkprofile.bat
@@ -0,0 +1,15 @@
+REM autor: Jose Moreno
+
+echo off
+mkdir %USERPROFILE%\asir  %USERPROFILE%\dam  %USERPROFILE%\smr  %USERPROFILE%\daw  %USERPROFILE%\ceti
+
+mkdir add %USERPROFILE%\dam\pracs  %USERPROFILE%\dam\docs	 %USERPROFILE%\dam\exam
+
+mkdir add %USERPROFILE%\asir\pracs	%USERPROFILE%\asir\docs	 %USERPROFILE%\asir\exam
+
+mkdir add %USERPROFILE%\daw\pracs	%USERPROFILE%\daw\docs	%USERPROFILE%\daw\exam
+
+mkdir add %USERPROFILE%\smr\pracs	%USERPROFILE%\smr\docs	%USERPROFILE%\smr\exam
+
+mkdir add %USERPROFILE%\ceti\pracs	%USERPROFILE%\ceti\docs	%USERPROFILE%\ceti\exam
+
