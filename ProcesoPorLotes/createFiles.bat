858d055603fe42be1ca5aa081750ce7262bc5335
joosemooreno10 <146065095+joosemooreno10@users.noreply.github.com>
2023-11-15 11:54:14 +0100

    Add files via upload

---
+++ ProcesoPorLotes/createFiles.bat
@@ -0,0 +1,19 @@
+@echo off
+REM Jose Moreno
+REM 15/11/2023
+
+title Condicionales
+echo pulsa 1 para crear archivo .txt
+echo pulsa 2 para crear un archivo .bat
+set /p opcion=
+if %opcion% == 1 goto txt
+if %opcion% == 2 goto bat
+
+:txt
+set /P a1="Introduce el nombre de tu archivo: "
+type nul > %a1%.txt
+exit
+:bat
+set /P a2="Introduce el nombre de tu archivo: "
+type nul > %a2%.bat
+exit
